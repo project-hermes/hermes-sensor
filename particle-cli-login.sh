c594b6a32559d5bec39ace9f25a7f74120929b3c
Brad Bazemore <bwbazemore@pendo.io>
2018-03-24 20:17:39 -0400

    Create particle-cli-login.sh

---
+++ particle-cli-login.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/expect
+log_user 0
+set timeout 10
+
+set user [lindex $argv 0]
+
+set password [lindex $argv 1]
+
+spawn /home/travis/.nvm/v0.10.36/bin/particle cloud login
+
+log_user 0
+
+expect "? Please enter your email address: "
+send "$user\r"
+
+log_user 0
+
+expect "? Please enter your password: "
+send "$password\r"
+
+interact
